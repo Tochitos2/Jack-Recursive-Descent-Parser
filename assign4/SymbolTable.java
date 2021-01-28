9a60836715daf28f5960a9cdeb3e785448786e52
Tochi <Tochitos2@gmail.com>
2021-01-28 12:48:27 +0000

    Added SymbolTable class.

---
+++ assign4/SymbolTable.java
@@ -0,0 +1,87 @@
+package assign4;
+
+import java.util.HashMap;
+
+public class SymbolTable {
+    private HashMap<String, Identity> classScope;
+    private HashMap<String, Identity> localScope;
+
+    /**
+     * Constructor
+     */
+    public SymbolTable() {
+        classScope = new HashMap<String, Identity>();
+        localScope = new HashMap<String, Identity>();
+    }
+
+    /**
+     * Reset local scope.
+     */
+    public void startSubroutine() {
+        localScope = new HashMap<String, Identity>();
+    }
+
+    /**
+     * Defines a new variable and store it at the appropriate scope.
+     * @param name The name of the variable.
+     * @param type The  data type of the variable.
+     * @param kind Whether the variable is static, a field, an argument, or a local variable.
+     */
+    public void define(String name, String type, Kind kind) {
+        Identity identity = new Identity(name, type, kind);
+
+        switch(kind) {
+            case STATIC, FIELD -> classScope.put(name, identity);
+            case ARG, VAR -> localScope.put(name, identity);
+        }
+    }
+
+    /**
+     * Returns whether variable of a given name is defined in the chosen scope.
+     * @param name
+     * @param scope
+     * @return
+     */
+    public boolean isDefined(String name, Scope scope) {
+        switch(scope) {
+            case CLASS -> { return classScope.containsKey(name); }
+            default -> { return localScope.containsKey(name); }
+        }
+    }
+
+    public Kind kindOf(String name, Scope scope) {
+        switch(scope) {
+            case CLASS -> { return classScope.get(name).kind; }
+            default -> { return localScope.get(name).kind; }
+        }
+    }
+
+    public String typeOf(String name, Scope scope) {
+        switch(scope) {
+            case CLASS -> { return classScope.get(name).type; }
+            default -> { return localScope.get(name).type; }
+        }
+    }
+
+
+    public enum Kind {
+        STATIC, FIELD, ARG, VAR,
+    }
+
+    public enum Scope {
+        CLASS, LOCAL,
+    }
+
+    class Identity {
+        public String name;
+        public String type;
+        public Kind kind;
+
+        public Identity(String name, String type, Kind kind) {
+            this.name = name;
+            this.type = type;
+            this.kind = kind;
+        }
+    }
+
+}
